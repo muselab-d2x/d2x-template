e8f69724c96f513d333487c6fd5a4591dc7f97fc
Jason Lantz <jason@muselab.com>
2023-09-09 12:33:32 -0500

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,6 @@
 import os
 import shutil
 
-{% if cookiecutter.use_robot == "n" %}
-shutil.rmtree("robot")
-{% endif %}
-
 {% if cookiecutter.project_type == "Unpackaged" %}
 os.remove(".github/workflows/beta.yml")
 {% endif %}
